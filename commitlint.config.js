75b3f183ce7bcf56d97faaab5b41f051f086432e
Alex Bates <16batesa@gmail.com>
2020-02-09 00:04:55 +0000

    ci: add commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            [
+                'build',
+                'chore',
+                'ci',
+                'docs',
+                'feat',
+                'fix',
+                'improve', // !!
+                'perf',
+                'refactor',
+                'revert',
+                'style',
+                'test'
+            ]
+        ],
+        // TODO: scope-enum
+    }
+}
